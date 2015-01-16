a10220e6d9e820315b6d36499692d4bd58d2b329
ghoshbinayak <ghoshbinayak@gmail.com>
2015-01-16 21:45:32 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='instruo',
+      version='0.1',
+      description='instruo website',
+      author='binayak, ananyo, abhas',
+      author_email='ghoshbinayak@gmail.com, ananyo.sen2@gmail.com, abhasbhattacharya2@gmail.com',
+      url='github.com/ghoshbinayak/instruo15',
+#      install_requires=['Django>=1.3'],
+     )
